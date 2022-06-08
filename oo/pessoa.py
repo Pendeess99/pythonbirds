49a6136a1cf7e756eeb2e5d89f0dba54c304ba0e
gabri <gabrielmendes10@outlook.com>
2022-06-07 22:39:08 -0300

    Criado atributo complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,21 @@
 class Pessoa:
+    def __init__(self, *filhos,nome=None, idade=23333):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    gabriel = Pessoa(nome='gabriel')
+    mendes = Pessoa(gabriel, nome='Mendes')
+    print(Pessoa.cumprimentar(mendes))
+    print(id(mendes))
+    print(mendes.cumprimentar())
+    print(mendes.nome)
+    print(mendes.idade)
+    for filho in mendes.filhos:
+        print(filho.nome)
+    print(mendes.filhos)
